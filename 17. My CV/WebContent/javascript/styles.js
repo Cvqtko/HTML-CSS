b00de22dcfabc1e1cefa8b546401d1abaebbe7bd
Cvqtko <cvqtkokirilov@gmail.com>
2019-11-20 21:04:38 +0200

    My CV

---
+++ 17. My CV/WebContent/javascript/styles.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', (event) => {
+  document.getElementsByTagName('body')[0].classList.add('loaded');
+});
+
+function myFunction() {
+	  var x = document.getElementById("my-navigation");
+	  if (x.className === "navigation") {
+	    x.className += " responsive";
+	  } else {
+	    x.className = "navigation";
+	  }
+}
