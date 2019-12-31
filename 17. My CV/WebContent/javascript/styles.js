aaa5076b618ac661e18aefbb326cbfdd6974d5fc
Cvqtko <cvqtkokirilov@gmail.com>
2019-12-31 15:11:15 +0200

    Moved the javascript to its own file

--- 17. My CV/WebContent/javascript/styles.js
+++ 17. My CV/WebContent/javascript/styles.js
@@ -10,4 +10,8 @@ function myFunction() {
 	    x.className = "navigation";
 	  }
 }
+function myLocation() {
+	window
+			.open("https://www.google.com/maps/place/ul.+%22Belmeken%22+2,+1618+Bakston,+Sofia/@42.6669158,23.2632387,14.5z/data=!4m5!3m4!1s0x40aa84cecc3febb7:0xf139452796a01acf!8m2!3d42.6699639!4d23.2712631");
+}
 
