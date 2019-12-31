e6cc7dcd423d728748003d2c0fca5ae47bf4a66a
Cvqtko <cvqtkokirilov@gmail.com>
2019-12-31 16:47:42 +0200

    Added iframe to location tab

--- 17. My CV/WebContent/javascript/styles.js
+++ 17. My CV/WebContent/javascript/styles.js
@@ -11,7 +11,12 @@ function myFunction() {
 	  }
 }
 function myLocation() {
-	window
-			.open("https://www.google.com/maps/place/ul.+%22Belmeken%22+2,+1618+Bakston,+Sofia/@42.6669158,23.2632387,14.5z/data=!4m5!3m4!1s0x40aa84cecc3febb7:0xf139452796a01acf!8m2!3d42.6699639!4d23.2712631");
+	document.getElementById('map').style.display='block';
+	document.getElementById('close').style.display='block';
+	document.getElementById('hide-map').style.display='none';
 }
-
+function closed(){
+	document.getElementById('map').style.display='none';
+	document.getElementById('close').style.display='none';
+	document.getElementById('hide-map').style.display='block';
+}
