5b6ec530163687772c2e0a442fc543ffeb3ad7f0
Cvqtko <cvqtkokirilov@gmail.com>
2020-01-04 21:36:22 +0200

    Added certificates section

--- 17. My CV/WebContent/javascript/functions.js
+++ 17. My CV/WebContent/javascript/functions.js
@@ -7,14 +7,12 @@ function showNavigation() {
 	    x.className = "navigation";
 	  }
 }
-function showMap() {
-	document.getElementById('map').style.display='block';
-	document.getElementById('close-map').style.display='block';
+function showElement(element) {
+	document.getElementById(element).style.display='block';
 
 }
-function hideMap(){
-	document.getElementById('map').style.display='none';
-	document.getElementById('close-map').style.display='none';
+function hideElement(element){
+	document.getElementById(element).style.display='none';
 }
 
 function sendEmail() 
